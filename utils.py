c23f9bc38b1b70ecbf2a74882bdbdf1e2e59e640
Maksim Koltiugin <mkoltugin@gmail.com>
2023-03-23 15:22:57 +0300

    split Pre-processing.ipynb into balanced and unbalanced files

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
+import numpy as np
 import torch
-import torch.nn as nn
 from torch.utils.data import Dataset
 from sklearn.metrics import f1_score
 from tqdm import tqdm
@@ -30,6 +30,13 @@ class CustomDataset(Dataset):
         return X, y
 
 
+def compute_weights_by_y_train(y_train):
+    _, counts = np.unique(y_train, return_counts=True)
+    total_weight = counts.sum()
+    weights = 1 / np.divide(counts, total_weight)
+    return torch.from_numpy(np.divide(weights, np.min(weights))).float().to(device)
+
+
 def draw(train_losses, test_losses, train_f1_scores, test_f1_scores):
     clear_output()
 
@@ -53,8 +60,7 @@ def draw(train_losses, test_losses, train_f1_scores, test_f1_scores):
     plt.show()
 
 
-def train(model, optimizer, train_loader, test_loader, epochs, saving_path, scheduler=None):
-    criterion = nn.NLLLoss()
+def train(model, optimizer, criterion, train_loader, test_loader, epochs, saving_path, scheduler=None):
     train_losses = []
     test_losses = []
     train_f1_scores = []
@@ -62,47 +68,49 @@ def train(model, optimizer, train_loader, test_loader, epochs, saving_path, sche
     best_f1 = 0
 
     for i in tqdm(range(epochs)):
-        train_loss_sum = 0
-        test_loss_sum = 0
-        train_f1_sum = 0
-        test_f1_sum = 0
+        try:
+            train_loss_sum = 0
+            test_loss_sum = 0
+            train_f1_sum = 0
+            test_f1_sum = 0
 
-        # train
-        model.train()
-        for X, y in iter(train_loader):
-            X, y = X.to(device), y.to(device)
-            y_pred = model(X.float())
-            loss = criterion(y_pred, y)
-            train_loss_sum += loss.item()
-            train_f1_sum += f1_score(y.cpu(), torch.argmax(y_pred, dim=1).cpu(), average='macro')
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        train_losses.append(train_loss_sum / len(train_loader))
-        train_f1_scores.append(train_f1_sum / len(train_loader))
-
-        # test
-        model.eval()
-        with torch.no_grad():
-            for X, y in iter(test_loader):
+            # train
+            model.train()
+            for X, y in iter(train_loader):
                 X, y = X.to(device), y.to(device)
                 y_pred = model(X.float())
                 loss = criterion(y_pred, y)
-                test_loss_sum += loss.item()
-                test_f1_sum += f1_score(y.cpu(), torch.argmax(y_pred, dim=1).cpu(), average='macro')
-            test_losses.append(test_loss_sum / len(test_loader))
-            test_f1_scores.append(test_f1_sum / len(test_loader))
+                train_loss_sum += loss.item()
+                train_f1_sum += f1_score(y.cpu(), torch.argmax(y_pred, dim=1).cpu(), average='macro')
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            train_losses.append(train_loss_sum / len(train_loader))
+            train_f1_scores.append(train_f1_sum / len(train_loader))
 
-        draw(train_losses, test_losses, train_f1_scores, test_f1_scores)
-        if scheduler is not None:
-            scheduler.step()
-        if test_f1_sum > best_f1:
-            best_f1 = test_f1_sum
-            torch.save(model.state_dict(), saving_path)
+            # test
+            model.eval()
+            with torch.no_grad():
+                for X, y in iter(test_loader):
+                    X, y = X.to(device), y.to(device)
+                    y_pred = model(X.float())
+                    loss = criterion(y_pred, y)
+                    test_loss_sum += loss.item()
+                    test_f1_sum += f1_score(y.cpu(), torch.argmax(y_pred, dim=1).cpu(), average='macro')
+                test_losses.append(test_loss_sum / len(test_loader))
+                test_f1_scores.append(test_f1_sum / len(test_loader))
+
+            draw(train_losses, test_losses, train_f1_scores, test_f1_scores)
+            if scheduler is not None:
+                scheduler.step()
+            if test_f1_sum > best_f1:
+                best_f1 = test_f1_sum
+                torch.save(model.state_dict(), saving_path)
+        except KeyboardInterrupt:
+            pass
 
 
-def train_with_int_input(model, optimizer, train_loader, test_loader, epochs, saving_path, scheduler=None):
-    criterion = nn.NLLLoss()
+def train_with_int_input(model, optimizer, criterion, train_loader, test_loader, epochs, saving_path, scheduler=None):
     train_losses = []
     test_losses = []
     train_f1_scores = []
@@ -110,40 +118,43 @@ def train_with_int_input(model, optimizer, train_loader, test_loader, epochs, sa
     best_f1 = 0
 
     for i in tqdm(range(epochs)):
-        train_loss_sum = 0
-        test_loss_sum = 0
-        train_f1_sum = 0
-        test_f1_sum = 0
+        try:
+            train_loss_sum = 0
+            test_loss_sum = 0
+            train_f1_sum = 0
+            test_f1_sum = 0
 
-        # train
-        model.train()
-        for X, y in iter(train_loader):
-            X, y = X.to(device), y.to(device)
-            y_pred = model(X)
-            loss = criterion(y_pred, y)
-            train_loss_sum += loss.item()
-            train_f1_sum += f1_score(y.cpu(), torch.argmax(y_pred, dim=1).cpu(), average='macro')
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        train_losses.append(train_loss_sum / len(train_loader))
-        train_f1_scores.append(train_f1_sum / len(train_loader))
-
-        # test
-        model.eval()
-        with torch.no_grad():
-            for X, y in iter(test_loader):
+            # train
+            model.train()
+            for X, y in iter(train_loader):
                 X, y = X.to(device), y.to(device)
                 y_pred = model(X)
                 loss = criterion(y_pred, y)
-                test_loss_sum += loss.item()
-                test_f1_sum += f1_score(y.cpu(), torch.argmax(y_pred, dim=1).cpu(), average='macro')
-            test_losses.append(test_loss_sum / len(test_loader))
-            test_f1_scores.append(test_f1_sum / len(test_loader))
+                train_loss_sum += loss.item()
+                train_f1_sum += f1_score(y.cpu(), torch.argmax(y_pred, dim=1).cpu(), average='macro')
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            train_losses.append(train_loss_sum / len(train_loader))
+            train_f1_scores.append(train_f1_sum / len(train_loader))
 
-        draw(train_losses, test_losses, train_f1_scores, test_f1_scores)
-        if scheduler is not None:
-            scheduler.step()
-        if test_f1_sum > best_f1:
-            best_f1 = test_f1_sum
-            torch.save(model.state_dict(), saving_path)
+            # test
+            model.eval()
+            with torch.no_grad():
+                for X, y in iter(test_loader):
+                    X, y = X.to(device), y.to(device)
+                    y_pred = model(X)
+                    loss = criterion(y_pred, y)
+                    test_loss_sum += loss.item()
+                    test_f1_sum += f1_score(y.cpu(), torch.argmax(y_pred, dim=1).cpu(), average='macro')
+                test_losses.append(test_loss_sum / len(test_loader))
+                test_f1_scores.append(test_f1_sum / len(test_loader))
+
+            draw(train_losses, test_losses, train_f1_scores, test_f1_scores)
+            if scheduler is not None:
+                scheduler.step()
+            if test_f1_sum > best_f1:
+                best_f1 = test_f1_sum
+                torch.save(model.state_dict(), saving_path)
+        except KeyboardInterrupt:
+            pass
